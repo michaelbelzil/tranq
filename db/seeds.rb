e96036f82d42d75b760c66e578b50aa34b330025
Michael Belzil <michaelbelzil@gmail.com>
2018-08-21 15:58:25 -0400

    seeds started

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 user1 = User.create(email: "user@email.com", password: "hunter2")
 user1.save
+user2 = User.create(email: "email@email.com", password: "hunter2")
+user2.save
+god = User.create(email: "god@heaven.com", password: "hunter2")
+god.save
+hat1 = Item.create(category: "head", photo: "https://www.freepngimg.com/download/hat/4-hat-png-image.png", user_id: "1")
+hat1.save
+hat2 = Item.create(category: "head", photo: "http://clipart-library.com/images/8iGb5EB8T.png", user_id: "1")
+hat2.save
+
