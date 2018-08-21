d1d6cab77a94bc0653bccbe641b1386444420932
sara <sarneijim@gmail.com>
2018-08-21 12:59:28 -0400

    hola

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,12 +1,36 @@
 $( "#feed_option" ).click(function() {
-  $( ".navbar" ).css("background-color", "red");
+  $(".navbar__specific__item").each(function(){
+    if($(this).hasClass("navbar__specific--feed")){
+      $(this).show();
+    }else{
+      $(this).hide()
+    }
+  });
 });
 $( "#decoration_option" ).click(function() {
-  $( ".navbar" ).css("background-color", "yellow");
+  $(".navbar__specific__item").each(function(){
+    if($(this).hasClass("navbar__specific--decorate")){
+      $(this).show();
+    }else{
+      $(this).hide()
+    }
+  });
 });
 $( "#friend_option" ).click(function() {
-  $( ".navbar" ).css("background-color", "blue");
+  $(".navbar__specific__item").each(function(){
+    if($(this).hasClass("navbar__specific--friend")){
+      $(this).show();
+    }else{
+      $(this).hide()
+    }
+  });
 });
 $( "#setting_option" ).click(function() {
-  $( ".navbar" ).css("background-color", "green");
+  $(".navbar__specific__item").each(function(){
+    if($(this).hasClass("navbar__specific--setting")){
+      $(this).show();
+    }else{
+      $(this).hide()
+    }
+  });
 });
