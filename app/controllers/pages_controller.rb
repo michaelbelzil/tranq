7f7a65efe79cc5b5b4621af55edae917e0c9a305
Michael Belzil <michaelbelzil@gmail.com>
2018-08-21 14:31:25 -0400

    login routing now works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       redirect_to tranq_path(current_user)
+    else
+      redirect_to new_user_session_path
     end
   end
 end
