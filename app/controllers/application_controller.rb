7808018b8bbdc960d3798db9d42fe281c00c547c
Sawyer Gilker <sawyer.gilker@hotmail.com>
2018-08-24 12:24:49 -0400

    login forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+    def after_sign_in_path_for(resource)
+      tranq_path(current_user.id)
+    end
 end
