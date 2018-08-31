bce40bb9e9ad0c8df05a4dd7fabe32a95088eba6
Konstantin Baumer <baumer.konstantin@gmail.com>
2018-08-31 13:21:49 -0400

    users starting off have the light turned on

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
   def starting_values
     self.happiness = 100
     self.last_fed = Time.now
-    self.tucked_in = true
+    self.tucked_in = false
 
     Item::CATEGORIES.each do |category|
       item = Item.find_by(category: category, owner: "blank")
